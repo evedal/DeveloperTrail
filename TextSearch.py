eb2d19f599a2d96f2ea65f91961e48c0fb67cd9b
evendalen <even.dalen@live.no>
2016-11-11 22:31:41 +0100

    Posting code for lempel_ziv compression

---
+++ TextSearch.py
@@ -0,0 +1,50 @@
+search_text = ['M','i','s','s','i','s','s','i','p','p','i']
+search_word = ['p','p','i']
+
+def naive_text_search(pattern, search_text):
+    for i in range(len(search_text) - len(pattern)):
+        for j in range(len(pattern)):
+            if search_text[i+j] != pattern[j]:
+                break
+        else:
+            return i
+    return -1
+
+def simplified_boyer_moore(pattern, search_text):
+    count = 0
+    while len(search_text)-len(pattern) >= count:
+        if search_text[count+len(pattern)-1] not in pattern:
+            count += len(pattern)
+            continue
+        for i in range(len(pattern)):
+            if search_text[count+i] != pattern[i]:
+                count += 1
+                break
+        else:
+            return count
+    return -1
+
+def boyer_moore(pattern, search_text):
+    count = 0
+    pattern_indexes = {}
+    for i, char in enumerate(reversed(pattern)):
+        if char not in pattern_indexes:
+            pattern_indexes.update({char:i})
+
+    while len(search_text)-len(pattern) >= count:
+        if search_text[count+len(pattern)-1] not in pattern_indexes:
+            count += len(pattern)
+            continue
+        else:
+            print(count)
+            count += pattern_indexes.get(search_text[count+len(pattern)-1])
+            print(pattern_indexes.get(search_text[count+len(pattern)-1]))
+            for i in range(len(pattern)):
+                print(search_text[count+i], pattern[i])
+                if search_text[count+i] != pattern[i]:
+                    count += 1
+                    break
+            else:
+                return count
+    return -1
+
